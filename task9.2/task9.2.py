da4b9a8b03285510f4237bc8493dc02b0435c0cb
Yuliia Orishchenko <orishchenko.ya.com>
2022-10-03 21:51:18 +0300

    added 9.2

---
+++ task9.2/task9.2.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.append(r"C:\Users\Asus\PycharmProjects\Homework9\task9.2\utils")
+
+import utils.length
+print(len("vhjh"))
+
---
+++ task9.2/utils/length.py
@@ -0,0 +1,2 @@
+def get_length(name):
+    return len(name)
